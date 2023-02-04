1228033c3a0f5031d01047d56e86a81e1bc4db77
Lionel <li.janin@laposte.net>
2023-02-04 12:31:16 +0100

    Create base character (move + jump) & add DoubleJump
    Added a charactercontroller 2d

---
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    private bool _grounded;
+
+    private BoxCollider2D _boxCollider;
+    
+    public bool IsGrounded => _grounded;
+
+    private Vector2 _velocity;
+
+    private void Awake()
+    {
+        _boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+
+    private void LateUpdate()
+    {
+        _grounded = false;
+        
+        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, _boxCollider.size, 0);
+        foreach (Collider2D hit in hits)
+        {
+            if (hit == _boxCollider)
+                continue;
+
+            ColliderDistance2D colliderDistance = hit.Distance(_boxCollider);
+
+            if (colliderDistance.isOverlapped)
+            {
+                transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
+            }
+            
+            if (Vector2.Angle(colliderDistance.normal, Vector2.up) < 90 && _velocity.y <= 0)
+            {
+                _grounded = true;
+            }
+        }
+    }
+
+    public void Move(Vector2 motion)
+    {
+        _velocity = motion;
+        transform.Translate(motion);
+    }
+}
