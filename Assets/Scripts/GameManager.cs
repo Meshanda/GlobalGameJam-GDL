eb1f48e54c1cb934bdb867012e151acec75847a8
Killian <32911550+Meshanda@users.noreply.github.com>
2023-02-04 19:36:20 +0100

    EndScreen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjects.Variables;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private StringVariable _endScreen;
+    
+    private void Win()
+    {
+        
+    }
+}
