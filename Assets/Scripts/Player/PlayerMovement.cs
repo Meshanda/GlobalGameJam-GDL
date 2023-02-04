483e80d3f147b94f6e0402f7f467d7e6a79ae8b1
Lionel <li.janin@laposte.net>
2023-02-04 13:46:50 +0100

    Added dash

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
 
 #region Unlockable
     [SerializeField] private BoolVariable secondJumpUnlocked;
+    [SerializeField] private BoolVariable dashUnlocked;
 #endregion
 
 #region Jump
@@ -34,8 +35,26 @@ public class PlayerMovement : MonoBehaviour
 #region SideMovement
     [Range(1, 50)]
     [SerializeField] private float moveSpeed;
+
+    private float _horizontalVelocity;
+    private bool _mooving;
+
+    #endregion
+
+#region Dash
+
+    private bool _dashing;
+    private bool _canDash;
+
+    private float _dashVelocity;
     
-    private Vector2 _horizontalAxis;
+    [Range(1,10)]
+    [SerializeField] private float dashPower;
+    
+    [SerializeField] private float dashTime;
+    
+    
+
 #endregion
     
 
@@ -43,32 +62,33 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private CharacterController2D controller;
 
 
-    
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        _canDash = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
-        GravityControll();
+        RotateControl();
+        GravityControl();
     }
 
     public void Move()
     {
-        Vector2 fall = new Vector2(0, _verticalVelocity);
+        Vector2 verticalAxis = new Vector2(0, _verticalVelocity);
+        Vector2 horizontalAxis = new Vector2(_horizontalVelocity, 0);
         
-        controller.Move((_horizontalAxis * (moveSpeed * Time.deltaTime)) + (fall * Time.deltaTime));
+        if (_dashVelocity != 0)
+            horizontalAxis.x = _dashVelocity;
+        
+        controller.Move((horizontalAxis * (moveSpeed * Time.deltaTime)) + (verticalAxis * Time.deltaTime));
     }
 
-    private void GravityControll()
+    private void GravityControl()
     {
-        if (_verticalVelocity <= 0)
+        if (_verticalVelocity <= 0 && _dashing == false)
         {
             _gravity = gravityDown;
         }
@@ -76,12 +96,26 @@ public class PlayerMovement : MonoBehaviour
         if (controller.IsGrounded && _verticalVelocity < 0)
         {
             _secondJumping = false;
+            _canDash = true;
             _verticalVelocity = 0f;
         }
         else
         {
             if (_verticalVelocity > maxFallSpeed)
                 _verticalVelocity += _gravity * Time.deltaTime;
+        }
+    }
+
+    private void RotateControl()
+    {
+        if (_mooving == true)
+        {
+            float  localScaleX = transform.localScale.x;
+            float  localScaleY = transform.localScale.y;
+            
+            if ((_horizontalVelocity > 0 && localScaleX < 0) || (_horizontalVelocity < 0 && localScaleX > 0))
+                transform.localScale = new Vector2(localScaleX * -1, localScaleY);
+            
         }
     }
     
@@ -104,13 +138,27 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private IEnumerator DoDash()
+    {
+        _canDash = false;
+        _dashing = true;
+        _gravity = 0;
+        _verticalVelocity = 0;
+        
+        _dashVelocity = transform.localScale.x * dashPower;
+        yield return new WaitForSeconds(dashTime);
+        
+        _dashing = false;
+        _dashVelocity = 0;
+    }
+
 #region InputsCall
     public void OnMove(InputValue value)
     {
         Vector2 move = value.Get<Vector2>();
-        
-        _horizontalAxis.x = move.x;
-        _horizontalAxis.Normalize();
+
+        _mooving = move.x != 0;
+        _horizontalVelocity = move.x;
     }
 
 
@@ -123,6 +171,12 @@ public class PlayerMovement : MonoBehaviour
     {
         if (_verticalVelocity > _minimumYVelocity)
             _verticalVelocity = _minimumYVelocity;
+    }
+
+    public void OnDash()
+    {
+        if(_canDash == true && dashUnlocked.value == true)
+            StartCoroutine(DoDash());
     }
     
 #endregion
